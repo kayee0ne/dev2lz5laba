b0a5c5118fc8e681a95b2d53bc3f57930c1729a1
Yakovlev Evgeniy <99040981+kayee0ne@users.noreply.github.com>
2022-12-16 11:46:14 +0300

    Add files via upload

---
+++ dev2lz/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Dev
+{
+    class Dev
+    {
+        static void Main()
+        {
+            Console.WriteLine("1 for +");
+            Console.WriteLine("2 for -");
+            Console.WriteLine("3 for *");
+            Console.WriteLine("4 for /");
+            var line = Console.ReadLine();
+            Console.WriteLine("insert symbols for operation");
+            var op = Console.ReadLine();
+
+            Console.WriteLine(logic.run(line, op));
+        }
+    }
+}
---
+++ dev2lz/logic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Dev;
+
+public class logic
+{
+    public static string run(string s1, string s2)
+    {
+        var calc = new Calc();
+        var zxc = s1.Split(' ');
+        var zxc1 = s2.Split(' ');
+
+        int x = int.Parse(zxc[0]);
+        int a = int.Parse(zxc1[0]);
+        int b = int.Parse(zxc1[1]);
+
+        if (x == 1)
+        {
+            return calc.Sum(a, b).ToString();
+        }
+
+        if (x == 2)
+        {
+            return calc.Subtract(a, b).ToString();
+        }
+
+        if (x == 3)
+        {
+            return calc.Mult(a, b).ToString();
+        }
+
+        if (x == 4)
+        {
+            if (b == 0)
+            {
+                return "We cant / on 0!!";
+            }
+            else
+            {
+                return calc.Div(a, b).ToString();
+            }
+        }
+
+        return "";
+    }
+}
